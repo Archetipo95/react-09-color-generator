42d69e1e08ea4154579fc995f44ff19fb2ce9800
Martin Masevski <martin.masevski@gmail.com>
2021-05-30 00:09:36 +0200

    Implemented

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+// you also can get hex value inside color obj
 
-export default SingleColor
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => setAlert(false), 2000);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 20 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
